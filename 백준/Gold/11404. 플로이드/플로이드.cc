28dd34af5f38bb5b558dc5e97982320bcf37907f
jina <103057334+wlsdk9803@users.noreply.github.com>
2024-02-23 01:50:00 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,43 +1,33 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int n, m; //n은 도시 개수, m은 버스 개수
-	const int max_cost = 10000001;
-	int adj[101][101];
-	fill(adj[0], adj[101], max_cost);
-	int dist[101][101];
-
-	cin >> n >> m;
+	int n, m; cin >> n >> m;
+	vector<vector<int>> adj(n + 1, vector<int>(n + 1, 10000001));
+	vector<vector<int>> dist(n + 1, vector<int>(n + 1));
 	for (int i = 0; i < m; i++) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		adj[a][b] = min(adj[a][b], c);
+		int u, v, w; cin >> u >> v >> w;
+		adj[u][v] = min(adj[u][v], w);
 	}
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (i == j) dist[i][j] = 0;
-			else if (adj[i][j] != max_cost) dist[i][j] = adj[i][j];
-			else dist[i][j] = max_cost;
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 1; j < n + 1; j++) {
+			if (i == j) continue;
+			else dist[i][j] = adj[i][j];
 		}
 	}
-
-	for (int k = 1; k <= n; k++) {
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-			}
+	for (int k = 1; k < n + 1; k++) {
+		for (int i = 1; i < n + 1; i++) {
+			for (int j = 1; j < n + 1; j++) dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
 		}
 	}
-	
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (dist[i][j] == max_cost) cout << 0 << " ";
-			else cout << dist[i][j] << " ";
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 1; j < n + 1; j++) {
+			if (dist[i][j] == 10000001) dist[i][j] = 0;
+			cout << dist[i][j] << " ";
 		}
 		cout << "\n";
 	}
