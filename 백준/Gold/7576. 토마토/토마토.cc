b48e80137c9647135b2b9126da389062d9e82ad1
Jina <103057334+wlsdk9803@users.noreply.github.com>
2024-04-29 21:33:58 +0900

    [Gold V] Title: 토마토, Time: 120 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,60 +1,58 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
 #include <queue>
+#include <algorithm>
+#include <climits>
+
 using namespace std;
 
 int M, N;
-int box[1001][1001];
-bool visited[1001][1001];
-queue<pair<int, int>> q; //행, 열
 int dy[] = { 0, 0, 1, -1 };
 int dx[] = { 1, -1, 0, 0 };
 
-bool isValid(int x, int y) {
-	return (x >= 0 && x < M&& y >= 0 && y < N);
-}
-
-void bfs() {
-	while (!q.empty()) {
-		int y = q.front().first;
-		int x = q.front().second;
-		q.pop();
-		if (!visited[y][x]) {
-			visited[y][x] = true;
-			for (int i = 0; i < 4; i++) {
-				int ny = y + dy[i];
-				int nx = x + dx[i];
-				if (box[ny][nx] == 0 && isValid(nx, ny) && !visited[ny][nx]) {
-					box[ny][nx] = box[y][x] + 1;
-					q.push({ ny, nx });
-				}
-			}
-		}
-	}
+bool isValid(int row, int col) {
+	return row >= 0 && row < N && col >= 0 && col < M;
 }
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	memset(box, 0, sizeof(box));
-	memset(visited, false, sizeof(visited));
 	cin >> M >> N;
+	vector<vector<int>> box(N, vector<int>(M));
+	vector<vector<bool>> visited(N, vector<bool>(M));
+	vector<vector<int>> dist(N, vector<int>(M, INT_MAX));
+	queue<pair<int, int>> q;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			cin >> box[i][j];
-			if (box[i][j] == 1) q.push({ i, j });
+			if (box[i][j] == 1) {
+				q.push({ i, j });
+				dist[i][j] = 0;
+				visited[i][j] = true;
+			}
 		}
 	}
-	bfs();
-	int num = 0;
+	int res = 0;
+	while (!q.empty()) {
+		int row = q.front().first;
+		int col = q.front().second; q.pop();
+		for (int i = 0; i < 4; i++) {
+			int nrow = row + dy[i];
+			int ncol = col + dx[i];
+			if (isValid(nrow, ncol) && box[nrow][ncol] == 0 && !visited[nrow][ncol]) {
+				dist[nrow][ncol] = min(dist[nrow][ncol], dist[row][col] + 1);
+				q.push({ nrow, ncol });
+				visited[nrow][ncol] = true;
+				res = max(res, dist[nrow][ncol]);
+			}
+		}
+	}
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			if (box[i][j] == 0) {
-				cout << -1;
-				return 0;
+			if (box[i][j] == 0 && dist[i][j] == INT_MAX) {
+				cout << -1; return 0;
 			}
-			if (box[i][j] > num) num = box[i][j];
 		}
 	}
-	cout << num - 1;
+	cout << res;
 	return 0;
 }
