93c8b7a01657672818d51c3ccd67a9e09950b30b
Jina <103057334+wlsdk9803@users.noreply.github.com>
2024-07-04 22:24:00 +0900

    [Gold V] Title: 사과나무, Time: 20 ms, Memory: 3080 KB -BaekjoonHub

--- 백준/Gold/20002. 사과나무/사과나무.cc
+++ 백준/Gold/20002. 사과나무/사과나무.cc
@@ -6,25 +6,24 @@ using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	long long sum = -1001;
+	long long res = -1001;
 	int n; cin >> n;
 	vector<vector<int>> v(n, vector<int>(n));
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) cin >> v[i][j];
 	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			int num = v[i][j];
-			sum = num < sum ? sum : num;
-
-			long long temp_sum = num;
-			for (int gap = 1; gap <= min(n - i - 1, n - j - 1); gap++) {
-				for (int k = 0; k <= gap; k++) temp_sum += v[i + k][j + gap];
-				for (int k = 0; k < gap; k++) temp_sum += v[i + gap][j + k];
-				sum = temp_sum < sum ? sum : temp_sum;
+	vector<vector<long long>> sum(n + 1, vector<long long>(n + 1));
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) sum[i][j] = sum[i - 1][j] + sum[i][j - 1] + v[i - 1][j - 1] - sum[i - 1][j - 1];
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			for (int k = 1; k <= min(i, j); k++) {
+				long long temp_sum = sum[i][j] - sum[i - k][j] - sum[i][j - k] + sum[i - k][j - k];
+				res = res > temp_sum ? res : temp_sum;
 			}
 		}
 	}
-	cout << sum;
+	cout << res;
 	return 0;
 }
