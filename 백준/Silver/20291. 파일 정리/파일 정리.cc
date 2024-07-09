85498f3bb51435e67dc8fd3dd558bc1311d221c7
Jina <103057334+wlsdk9803@users.noreply.github.com>
2024-07-09 21:32:29 +0900

    [Silver III] Title: 파일 정리, Time: 164 ms, Memory: 18092 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.cc
+++ 백준/Silver/20291. 파일 정리/파일 정리.cc
@@ -2,19 +2,9 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
-#include <sstream>
 #include <algorithm>
 
 using namespace std;
-
-vector<string> split(string str, char delimiter) {
-    istringstream iss(str);
-    string buffer;
-    vector<string> result;;
-
-    while (getline(iss, buffer, delimiter)) result.push_back(buffer);
-    return result;
-}
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -24,7 +14,8 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         string file; cin >> file;
-        string extension = split(file, '.')[1];
+        int pos = file.find_last_of('.');
+        string extension = file.substr(pos + 1);
         if (um.find(extension) == um.end()) {
             um.insert({ extension, 1 });
             v.push_back(extension);
