a2d07ebbdbb0257333e34dce27756d6b5dcdad06
Jina <103057334+wlsdk9803@users.noreply.github.com>
2024-07-11 14:02:48 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 92 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, p; cin >> n >> p;
+    vector<priority_queue<int>> v(n + 1);
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        int string_num, fret_num;
+        cin >> string_num >> fret_num;
+        if (v[string_num].empty() || fret_num > v[string_num].top()) {
+            v[string_num].push(fret_num);
+            res++;
+            continue;
+        }
+        if (!v[string_num].empty() && fret_num == v[string_num].top()) continue;
+        while (!v[string_num].empty() && fret_num < v[string_num].top()) {
+            v[string_num].pop();
+            res++;
+        }
+        if (v[string_num].empty() || fret_num > v[string_num].top()) {
+            v[string_num].push(fret_num);
+            res++;
+        }
+    }
+    cout << res;
+    return 0;
+}
