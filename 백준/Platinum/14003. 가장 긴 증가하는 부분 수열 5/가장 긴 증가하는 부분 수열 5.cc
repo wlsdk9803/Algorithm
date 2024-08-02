8076850cd7815661e1fd0d79cc63b73aebf8ddf5
Jina <103057334+wlsdk9803@users.noreply.github.com>
2024-08-02 23:35:12 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 204 ms, Memory: 22444 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int N; cin >> N;
+	vector<int> v(N + 1);
+	vector<int> sub(1, -1000000001);
+	vector<int> idx(1);
+	for (int i = 1; i <= N; i++) cin >> v[i];
+	for (int i = 1; i <= N; i++) {
+		if (sub.back() < v[i]) sub.push_back(v[i]);
+		int index = lower_bound(sub.begin(), sub.end(), v[i]) - sub.begin();
+		if (v[i] < sub[index]) sub[index] = v[i];
+		idx.push_back(index);
+	}
+	vector<int> temp;
+	int n = sub.size() - 1;
+	cout << n << "\n";
+	for (int i = idx.size() - 1; i > 0; i--) {
+		if (idx[i] == n) {
+			temp.push_back(v[i]);
+			n--;
+		}
+	}
+	for (int i = temp.size() - 1; i >= 0; i--) cout << temp[i] << " ";
+    return 0;
+}
