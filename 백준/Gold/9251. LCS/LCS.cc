d9b960744b0c0cc381ddd17aae48692f8712c30e
Jina <103057334+wlsdk9803@users.noreply.github.com>
2024-08-13 21:36:52 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,18 +1,18 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	string str1, str2; cin >> str1 >> str2;
-	int n = str2.length(), m = str1.length();
+	int n = str1.length(), m = str2.length();
 	vector<vector<int>> v(n + 1, vector<int>(m + 1));
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= m; j++) {
-			if (str1[j - 1] == str2[i - 1]) v[i][j] = v[i - 1][j - 1] + 1;
+			if (str1[i - 1] == str2[j - 1]) v[i][j] = v[i - 1][j - 1] + 1;
 			else v[i][j] = max(v[i - 1][j], v[i][j - 1]);
 		}
 	}
