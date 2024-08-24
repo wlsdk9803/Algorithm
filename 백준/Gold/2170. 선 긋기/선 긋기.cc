970163e9368ae6c8f5078be0d402589c198bc18e
Jina <103057334+wlsdk9803@users.noreply.github.com>
2024-08-25 02:19:33 +0900

    [Gold V] Title: 선 긋기, Time: 348 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	vector<pair<int, int>> v; // 끝 점 위치 저장
+	int N; cin >> N;
+	for (int i = 0; i < N; i++) {
+		int a, b; cin >> a >> b;
+		v.push_back({ a, b });
+	}
+	sort(v.begin(), v.end());
+	vector<pair<int, int>> res;
+	res.push_back(v[0]);
+	for (int i = 1; i < N; i++) {
+		int res_front = res.back().first;
+		int res_back = res.back().second;
+		int a = v[i].first;
+		int b = v[i].second;
+		if (a == res_front || (a <= res_back && b > res_back)) res[res.size() - 1].second = b;
+		else if (a > res_back) res.push_back({ a, b });
+	}
+	int total_length = 0;
+	for (int i = 0; i < res.size(); i++) total_length += res[i].second - res[i].first;
+	cout << total_length;
+	return 0;
+}
