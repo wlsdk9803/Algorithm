0fda73d8967c1366677d93626a6466638e9894f3
Jina <103057334+wlsdk9803@users.noreply.github.com>
2024-08-25 17:48:06 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,6 +1,4 @@
--- 1번 형질: 2로 나눈 나머지가 1이면 있음
--- 2번 형질: 4로 나눈 나머지가 2 또는 3이면 있음
--- 3번 형질: 8로 나눈 나머지가 7, 6, 5, 4이면 있음
+#SELECT ID, BIN(GENOTYPE) FROM ECOLI_DATA;
 
-SELECT COUNT(*) AS COUNT FROM ECOLI_DATA
-WHERE GENOTYPE % 4 NOT IN (2, 3) AND (GENOTYPE % 2 = 1 OR GENOTYPE % 8 IN (4, 5, 6, 7));
+SELECT COUNT(ID) AS COUNT FROM ECOLI_DATA
+WHERE RIGHT(BIN(GENOTYPE), 3) IN ("101", "001", "100", "01", "1");
