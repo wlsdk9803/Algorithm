00a434f7649c58fcc3a69f5069e6c296275a9ff6
Jina <103057334+wlsdk9803@users.noreply.github.com>
2024-09-27 15:51:01 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 12 ms, Memory: 3716 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -1,67 +1,49 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
+
 using namespace std;
-
-const int maxnum = 100001;
-bool visited[maxnum] = { false };
-
-vector<int> path;
-
-int bfs(int start, int end) {
-	int parent[maxnum];
-	queue<pair<int, int>> q;
-	q.push({ start, 0 });
-	visited[start] = true;
-
-	while (!q.empty()) {
-		int cur = q.front().first;
-		int start_to_cur = q.front().second;
-		q.pop();
-		if (cur == end) {
-			path.push_back(cur);
-			int idx = cur;
-			while (idx != start) {
-				path.push_back(parent[idx]);
-				idx = parent[idx];
-			}
-			return start_to_cur;
-		}
-
-		for (int i = 0; i < 3; i++) {
-			int next, start_to_next;
-			switch (i) {
-			case(0):
-				next = cur - 1;
-				start_to_next = start_to_cur + 1;
-				break;
-			case(1):
-				next = cur + 1;
-				start_to_next = start_to_cur + 1;
-				break;
-			case(2):
-				next = cur * 2;
-				start_to_next = start_to_cur + 1;
-				break;
-			default:
-				break;
-			}
-			if (next >= 0 && next < maxnum && !visited[next]) {
-				parent[next] = cur;
-				visited[next] = true;
-				q.push({ next, start_to_next });
-			}
-		}
-	}
-	return -1;
-}
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	int N, K; cin >> N >> K;
-	cout << bfs(N, K) << "\n";
-	for (int i = path.size() - 1; i >= 0; i--) {
-		cout << path[i] << " ";
+	vector<bool> visited(100001);
+	vector<int> dist(100001, 1 << 30);
+	vector<int> before_node(100001);
+	queue<int> q;
+	q.push(N);
+	visited[N] = true;
+	dist[N] = 0;
+	vector<int> res_path;
+	while (!q.empty()) {
+		int cur = q.front(); q.pop();
+		int next = 0;
+		if (cur == K) {
+			cout << dist[cur] << "\n";
+			res_path.push_back(cur);
+			int prev = before_node[cur];
+			if (cur == N) break;
+			while (true) {
+				res_path.push_back(prev);
+				if (prev == N) break;
+				prev = before_node[prev];
+			}
+			break;
+		}
+		for (int i = 0; i < 3; i++) {
+			if (i == 0) next = cur - 1;
+			else if (i == 1) next = cur + 1;
+			else next = 2 * cur;
+			if (next >= 0 && next <= 100000 && !visited[next]) {
+				if (dist[cur] + 1 < dist[next]) {
+					q.push(next);
+					visited[next] = true;
+					dist[next] = dist[cur] + 1;
+					before_node[next] = cur;
+				}
+			}
+		}
 	}
+	for (int i = res_path.size() - 1; i >= 0; i--) cout << res_path[i] << " ";
 	return 0;
 }
