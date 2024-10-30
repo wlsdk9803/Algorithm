138dca08c8d3824fba2067996c0a3d683610592d
Jina <103057334+wlsdk9803@users.noreply.github.com>
2024-10-30 21:20:59 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,6 +1,6 @@
 SELECT ANIMAL_ID, NAME, 
     CASE
-        WHEN SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+        WHEN SEX_UPON_INTAKE REGEXP 'Neutered|Spayed' THEN 'O'
         ELSE 'X'
     END AS '중성화'
 FROM ANIMAL_INS
